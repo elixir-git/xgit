4b7f35419b3511112090d6e9f47ec708e343fde3
Eric Scouten <eric@scouten.com>
2019-07-11 22:21:04 -0700

    Add a simple folder diff module for use in testing. (#3)

---
+++ test/support/folder_diff.ex
@@ -0,0 +1,91 @@
+defmodule FolderDiff do
+  @moduledoc false
+  # Split out as a separate package? Compare two folders. Assert if mismatched.
+
+  import ExUnit.Assertions
+
+  def assert_folders_are_equal(folder1, folder2) do
+    files1 = folder1 |> File.ls!() |> Enum.sort()
+    files2 = folder2 |> File.ls!() |> Enum.sort()
+
+    assert_folders_are_equal(folder1, folder2, files1, files2)
+  end
+
+  defp assert_folders_are_equal(folder1, folder2, [file1 | files1], [file2 | files2]) do
+    cond do
+      file1 == file2 ->
+        assert_files_are_equal(folder1, folder2, file1)
+        assert_folders_are_equal(folder1, folder2, files1, files2)
+
+      file1 < file2 ->
+        flunk_file_missing(folder1, folder2, file1)
+
+      true ->
+        flunk_file_missing(folder2, folder1, file2)
+    end
+  end
+
+  defp assert_folders_are_equal(folder1, folder2, [file1 | _], []),
+    do: flunk_file_missing(folder1, folder2, file1)
+
+  defp assert_folders_are_equal(folder1, folder2, [], [file2 | _]),
+    do: flunk_file_missing(folder2, folder1, file2)
+
+  defp assert_folders_are_equal(_folder1, _folder2, [], []), do: :ok
+
+  defp assert_files_are_equal(_folder1, _folder2, "."), do: :ok
+  defp assert_files_are_equal(_folder1, _folder2, ".."), do: :ok
+
+  defp assert_files_are_equal(folder1, folder2, file) do
+    f1 = Path.join(folder1, file)
+    f2 = Path.join(folder2, file)
+
+    f1_is_dir? = File.dir?(f1)
+    f2_is_dir? = File.dir?(f2)
+
+    cond do
+      f1_is_dir? and f2_is_dir? -> assert_folders_are_equal(f1, f2)
+      f1_is_dir? -> flunk("#{f1} is a directory; #{f2} is a file")
+      f2_is_dir? -> flunk("#{f1} is a file; #{f2} is a directory")
+      true -> assert_flat_files_are_equal(f1, f2)
+    end
+  end
+
+  defp flunk_file_missing(folder_present, folder_missing, file) do
+    flunk("File #{file} exists in folder #{folder_present}, but is missing in #{folder_missing}")
+  end
+
+  defp assert_flat_files_are_equal(f1, f2) do
+    c1 = File.read!(f1)
+    c2 = File.read!(f2)
+
+    unless c1 == c2 do
+      c1 = truncate(c1)
+      c2 = truncate(c2)
+
+      flunk(~s"""
+      Files mismatch:
+
+      #{f1}:
+      #{c1}
+
+      #{f2}:
+      #{c2}
+
+      """)
+    end
+  end
+
+  defp truncate(c) do
+    length = String.length(c)
+
+    if length > 500 do
+      ~s"""
+      #{length} bytes starting with:
+      #{String.slice(c, 0, 500)}
+      """
+    else
+      c
+    end
+  end
+end
---
+++ test/xgit/support/folder_diff_test.exs
@@ -0,0 +1,208 @@
+defmodule FolderDiffTest do
+  use ExUnit.Case
+
+  import FolderDiff
+
+  setup do
+    Temp.track!()
+    :ok
+  end
+
+  test "happy path" do
+    t = Temp.mkdir!()
+
+    t
+    |> Path.join("HEAD")
+    |> File.write!("ref: refs/heads/master\n")
+
+    t
+    |> Path.join("hooks")
+    |> File.mkdir_p!()
+
+    assert_folders_are_equal(t, t)
+  end
+
+  test "failure: file missing on one side" do
+    f1 = Temp.mkdir!()
+
+    f1
+    |> Path.join("HEAD")
+    |> File.write!("ref: refs/heads/master\n")
+
+    f1
+    |> Path.join("hooks")
+    |> File.mkdir_p!()
+
+    f2 = Temp.mkdir!()
+
+    f2
+    |> Path.join("hooks")
+    |> File.mkdir_p!()
+
+    try do
+      assert_folders_are_equal(f1, f2)
+    rescue
+      error in [ExUnit.AssertionError] ->
+        assert error.message == "File HEAD exists in folder #{f1}, but is missing in #{f2}"
+    end
+
+    try do
+      assert_folders_are_equal(f2, f1)
+    rescue
+      error in [ExUnit.AssertionError] ->
+        assert error.message == "File HEAD exists in folder #{f1}, but is missing in #{f2}"
+    end
+  end
+
+  test "failure: last file (sorted) missing on one side" do
+    f1 = Temp.mkdir!()
+
+    f1
+    |> Path.join("HEAD")
+    |> File.write!("ref: refs/heads/master\n")
+
+    f1
+    |> Path.join("Books")
+    |> File.mkdir_p!()
+
+    f2 = Temp.mkdir!()
+
+    f2
+    |> Path.join("Books")
+    |> File.mkdir_p!()
+
+    try do
+      assert_folders_are_equal(f1, f2)
+    rescue
+      error in [ExUnit.AssertionError] ->
+        assert error.message == "File HEAD exists in folder #{f1}, but is missing in #{f2}"
+    end
+
+    try do
+      assert_folders_are_equal(f2, f1)
+    rescue
+      error in [ExUnit.AssertionError] ->
+        assert error.message == "File HEAD exists in folder #{f1}, but is missing in #{f2}"
+    end
+  end
+
+  test "failure: files mismatch" do
+    f1 = Temp.mkdir!()
+
+    f1
+    |> Path.join("HEAD")
+    |> File.write!("ref: refs/heads/master\n")
+
+    f1
+    |> Path.join("hooks")
+    |> File.mkdir_p!()
+
+    f2 = Temp.mkdir!()
+
+    f2
+    |> Path.join("HEAD")
+    |> File.write!("ref: refs/heads/mumble\n")
+
+    f2
+    |> Path.join("hooks")
+    |> File.mkdir_p!()
+
+    try do
+      assert_folders_are_equal(f1, f2)
+    rescue
+      error in [ExUnit.AssertionError] ->
+        assert error.message == ~s"""
+               Files mismatch:
+
+               #{f1}/HEAD:
+               ref: refs/heads/master
+
+
+               #{f2}/HEAD:
+               ref: refs/heads/mumble
+
+
+               """
+    end
+
+    try do
+      assert_folders_are_equal(f2, f1)
+    rescue
+      error in [ExUnit.AssertionError] ->
+        assert error.message == ~s"""
+               Files mismatch:
+
+               #{f2}/HEAD:
+               ref: refs/heads/mumble
+
+
+               #{f1}/HEAD:
+               ref: refs/heads/master
+
+
+               """
+    end
+  end
+
+  test "failure: truncated long string" do
+    f1 = Temp.mkdir!()
+
+    f1
+    |> Path.join("HEAD")
+    |> File.write!(~s"""
+    12345678901234567890123456789012345678901234567890
+    12345678901234567890123456789012345678901234567890
+    12345678901234567890123456789012345678901234567890
+    12345678901234567890123456789012345678901234567890
+    12345678901234567890123456789012345678901234567890
+    12345678901234567890123456789012345678901234567890
+    12345678901234567890123456789012345678901234567890
+    12345678901234567890123456789012345678901234567890
+    12345678901234567890123456789012345678901234567890
+    12345678901234567890123456789012345678901234567890
+    12345678901234567890123456789012345678901234567890
+    """)
+
+    f1
+    |> Path.join("hooks")
+    |> File.mkdir_p!()
+
+    f2 = Temp.mkdir!()
+
+    f2
+    |> Path.join("HEAD")
+    |> File.write!("ref: refs/heads/mumble\n")
+
+    f2
+    |> Path.join("hooks")
+    |> File.mkdir_p!()
+
+    try do
+      assert_folders_are_equal(f1, f2)
+    rescue
+      error in [ExUnit.AssertionError] ->
+        assert error.message == ~s"""
+               Files mismatch:
+
+               #{f1}/HEAD:
+               561 bytes starting with:
+               12345678901234567890123456789012345678901234567890
+               12345678901234567890123456789012345678901234567890
+               12345678901234567890123456789012345678901234567890
+               12345678901234567890123456789012345678901234567890
+               12345678901234567890123456789012345678901234567890
+               12345678901234567890123456789012345678901234567890
+               12345678901234567890123456789012345678901234567890
+               12345678901234567890123456789012345678901234567890
+               12345678901234567890123456789012345678901234567890
+               12345678901234567890123456789012345678901
+
+
+               #{f2}/HEAD:
+               ref: refs/heads/mumble
+
+
+               """
+    end
+  end
+end
