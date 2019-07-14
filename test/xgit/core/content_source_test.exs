021a93b91ebb3ee49b9a8c99200c83ffb440e4fc
Eric Scouten <eric@scouten.com>
2019-07-13 23:03:40 -0700

    Define a protocol for reading object content from various sources. (#10)

---
+++ test/xgit/core/content_source_test.exs
@@ -0,0 +1,27 @@
+defmodule Xgit.Core.ContentSourceTest do
+  use ExUnit.Case, async: true
+
+  alias Xgit.Core.ContentSource
+
+  describe "implementation for list" do
+    test "length/1" do
+      assert ContentSource.length('1234') == 4
+    end
+
+    test "stream/1" do
+      assert ContentSource.stream('1234') == '1234'
+    end
+  end
+
+  describe "implementation for string" do
+    test "length/1" do
+      assert ContentSource.length("1234") == 4
+      assert ContentSource.length("1ü34") == 5
+    end
+
+    test "stream/1" do
+      assert ContentSource.stream("1234") == '1234'
+      assert ContentSource.stream("1ü34") == [49, 195, 188, 51, 52]
+    end
+  end
+end
