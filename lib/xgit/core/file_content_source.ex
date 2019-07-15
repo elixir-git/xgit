24b04d23c339b0e465618fc4a80850bc641b2ee8
Eric Scouten <eric@scouten.com>
2019-07-14 20:57:00 -0700

    Implement Xgit.Core.FileContentSource.

---
+++ lib/xgit/core/file_content_source.ex
@@ -0,0 +1,41 @@
+defmodule Xgit.Core.FileContentSource do
+  @moduledoc ~S"""
+  Implements `Xgit.Core.ContentSource` to read content from a file on disk.
+  """
+
+  @typedoc ~S"""
+  Describes a file on disk which will be used for reading content.
+  """
+  @type t :: %__MODULE__{
+          path: Path.t(),
+          size: non_neg_integer | :file_not_found
+        }
+
+  @enforce_keys [:path, :size]
+  defstruct [:path, :size]
+
+  @doc ~S"""
+  Creates an `Xgit.Core.FileContentSource` for a file on disk.
+  """
+  @spec new(path :: Path.t()) :: t
+  def new(path) when is_binary(path) do
+    size =
+      case File.stat(path) do
+        {:ok, %File.Stat{size: size}} -> size
+        _ -> :file_not_found
+      end
+
+    %__MODULE__{path: path, size: size}
+  end
+
+  defimpl Xgit.Core.ContentSource do
+    alias Xgit.Core.FileContentSource, as: FCS
+    @impl true
+    def length(%FCS{size: :file_not_found}), do: raise("file not found")
+    def length(%FCS{size: size}), do: size
+
+    @impl true
+    def stream(%FCS{size: :file_not_found}), do: raise("file not found")
+    def stream(%FCS{path: path}), do: File.stream!(path, [:charlist], 2048)
+  end
+end
---
+++ test/xgit/core/file_content_source_test.exs
@@ -0,0 +1,53 @@
+defmodule Xgit.Core.ContentSource.FileTest do
+  use ExUnit.Case, async: true
+
+  alias Xgit.Core.ContentSource
+  alias Xgit.Core.FileContentSource
+
+  describe "implementation for file that exists" do
+    setup do
+      Temp.track!()
+      t = Temp.mkdir!()
+      path = Path.join(t, "example")
+      File.write!(path, "example")
+      {:ok, path: path}
+    end
+
+    test "length/1", %{path: path} do
+      fcs = FileContentSource.new(path)
+      assert ContentSource.length(fcs) == 7
+    end
+
+    test "stream/1", %{path: path} do
+      fcs = FileContentSource.new(path)
+
+      assert %File.Stream{} = stream = ContentSource.stream(fcs)
+      assert Enum.to_list(stream) == ['example']
+    end
+  end
+
+  describe "implementation for file that doesn't exist" do
+    setup do
+      Temp.track!()
+      t = Temp.mkdir!()
+      path = Path.join(t, "example")
+      {:ok, path: path}
+    end
+
+    test "length/1", %{path: path} do
+      fcs = FileContentSource.new(path)
+
+      assert_raise RuntimeError, "file not found", fn ->
+        ContentSource.length(fcs)
+      end
+    end
+
+    test "stream/1", %{path: path} do
+      fcs = FileContentSource.new(path)
+
+      assert_raise RuntimeError, "file not found", fn ->
+        ContentSource.stream(fcs)
+      end
+    end
+  end
+end
