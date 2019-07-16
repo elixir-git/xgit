15bd69502e162750c0045e9c07b6f5bf270f3932
Eric Scouten <eric@scouten.com>
2019-07-15 19:53:55 -0700

    Add ObjectType.valid?/1.

--- test/xgit/core/object_type_test.exs
+++ test/xgit/core/object_type_test.exs
@@ -2,27 +2,37 @@ defmodule Xgit.Core.ObjectTypeTest do
   use ExUnit.Case, async: true
   use Xgit.Core.ObjectType
 
-  @object_types [:blob, :tree, :commit, :tag]
+  @valid_object_types [:blob, :tree, :commit, :tag]
+  @invalid_object_types [:mumble, 1, "blob", 'blob', %{blob: true}, {:blob}, self()]
 
   defp accepted_object_type?(t) when is_object_type(t), do: true
   defp accepted_object_type?(_), do: false
 
+  describe "valid?/1" do
+    test "accepts known object types" do
+      for t <- @valid_object_types do
+        assert ObjectType.valid?(t)
+      end
+    end
+
+    test "rejects invalid values" do
+      for t <- @invalid_object_types do
+        refute ObjectType.valid?(t)
+      end
+    end
+  end
+
   describe "is_object_type/1" do
     test "accepts known object types" do
-      for t <- @object_types do
+      for t <- @valid_object_types do
         assert accepted_object_type?(t)
       end
     end
 
     test "rejects invalid values" do
-      refute accepted_object_type?(:mumble)
-      refute accepted_object_type?(1)
-      refute accepted_object_type?("blob")
-      refute accepted_object_type?('blob')
-      refute accepted_object_type?(%{blob: true})
-      refute accepted_object_type?({:blob})
-      refute accepted_object_type?(fn -> :blob end)
-      refute accepted_object_type?(self())
+      for t <- @invalid_object_types do
+        refute accepted_object_type?(t)
+      end
     end
   end
 end
