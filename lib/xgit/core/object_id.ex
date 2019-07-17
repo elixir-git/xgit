6602bf7afd62cbee0d214f411978e4fb1bd5235f
Eric Scouten <eric@scouten.com>
2019-07-16 20:18:02 -0700

    Implement ObjectId.calculate_id/2. (#31)

--- lib/xgit/core/object_id.ex
+++ lib/xgit/core/object_id.ex
@@ -5,6 +5,9 @@ defmodule Xgit.Core.ObjectId do
   This string must match the format for a SHA-1 hash (i.e. 40 characters
   of lowercase hex).
   """
+  use Xgit.Core.ObjectType
+
+  alias Xgit.Core.ContentSource
 
   @typedoc "A string containing 40 bytes of lowercase hex digits."
   @type t :: String.t()
@@ -25,4 +28,38 @@ defmodule Xgit.Core.ObjectId do
 
   def valid?(s) when is_binary(s), do: String.length(s) == 40 && String.match?(s, ~r/^[0-9a-f]+$/)
   def valid?(_), do: false
+
+  @doc ~S"""
+  Assign an object ID for a given data blob.
+
+  ## Parameters
+
+  * `data` describes how to read the data. (See `Xgit.Core.ContentSource`.)
+  * `type` is the intended git object type for this data. (See `Xgit.Core.ObjectType.`)
+  """
+  @spec calculate_id(data :: ContentSource.t(), type :: ObjectType.t()) :: ObjectId.t()
+  def calculate_id(data, type) when not is_nil(data) and is_object_type(type) do
+    size = ContentSource.length(data)
+
+    # Erlang/Elixir :sha == SHA-1
+    :sha
+    |> :crypto.hash_init()
+    |> :crypto.hash_update('#{type}')
+    |> :crypto.hash_update(' ')
+    |> :crypto.hash_update('#{size}')
+    |> :crypto.hash_update([0])
+    |> hash_update(ContentSource.stream(data))
+    |> :crypto.hash_final()
+    |> Base.encode16()
+    |> String.downcase()
+  end
+
+  defp hash_update(crypto_state, data) when is_list(data),
+    do: :crypto.hash_update(crypto_state, data)
+
+  defp hash_update(crypto_state, data) do
+    Enum.reduce(data, crypto_state, fn item, crypto_state ->
+      :crypto.hash_update(crypto_state, item)
+    end)
+  end
 end
--- test/xgit/core/object_id_test.exs
+++ test/xgit/core/object_id_test.exs
@@ -1,6 +1,7 @@
 defmodule Xgit.Core.ObjectIdTest do
   use ExUnit.Case, async: true
 
+  alias Xgit.Core.FileContentSource
   alias Xgit.Core.ObjectId
 
   test "zero/0" do
@@ -19,5 +20,42 @@ defmodule Xgit.Core.ObjectIdTest do
     refute ObjectId.valid?("1234567890abXdef12341234567890abcdef1234")
 
     refute ObjectId.valid?(nil)
+  end
+
+  describe "calculate_id/3" do
+    test "happy path: SHA hash with string content" do
+      assert ObjectId.calculate_id("test content\n", :blob) ==
+               "d670460b4b4aece5915caf5c68d12f560a9fe3e4"
+    end
+
+    test "happy path: deriving SHA hash from file on disk" do
+      Temp.track!()
+      path = Temp.path!()
+
+      content =
+        1..1000
+        |> Enum.map(fn _ -> "foobar" end)
+        |> Enum.join()
+
+      File.write!(path, content)
+
+      {output, 0} = System.cmd("git", ["hash-object", path])
+      expected_object_id = String.trim(output)
+
+      fcs = FileContentSource.new(path)
+      assert ObjectId.calculate_id(fcs, :blob) == expected_object_id
+    end
+
+    test "error: content nil" do
+      assert_raise FunctionClauseError, fn ->
+        ObjectId.calculate_id(nil, :blob)
+      end
+    end
+
+    test "error: :type invalid" do
+      assert_raise FunctionClauseError, fn ->
+        ObjectId.calculate_id("test content\n", :bogus)
+      end
+    end
   end
 end
