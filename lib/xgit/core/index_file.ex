c41b101b15e59977335cd514a4211df995a7ec77
Eric Scouten <eric@scouten.com>
2019-08-11 13:57:41 -0700

    Implement Xgit.Core.IndexFile module. (#65)
    * Define IndexFile struct.
    
    * Happy path: Read index file version 2.
    
    * More thorough testing of reading index files.
    
    * Punt on understanding extensions and trailing checksum for now.

---
+++ lib/xgit/core/index_file.ex
@@ -0,0 +1,306 @@
+defmodule Xgit.Core.IndexFile do
+  @moduledoc ~S"""
+  The index file records the current (intended) contents of a working tree
+  when last scanned or created by git.
+
+  In Xgit, the `IndexFile` structure is an abstract, in-memory data structure
+  without any tie to a specific persistence mechanism. Persistence is implemented
+  by a specific implementation of the `Xgit.Repository` behaviour.
+
+  Changes in the working tree can be detected by comparing the modification times
+  to the cached modification time within the index file.
+
+  Index files are also used during merges, where the merge happens within the
+  index file first, and the working directory is updated as a post-merge step.
+  Conflicts are stored in the index file to allow tool (and human) based
+  resolutions to be easily performed.
+  """
+
+  use Bitwise
+  use Xgit.Core.FileMode
+
+  alias Xgit.Util.NB
+
+  @typedoc ~S"""
+  Version number for an index file.
+  """
+  @type version :: 2..4
+
+  @typedoc ~S"""
+  This struct describes an entire working tree as understood by git.
+
+  ## Struct Members
+
+  * `:version`: the version number as read from disk (typically 2, 3, or 4)
+  * `:entry_count`: the number of items in `entries`
+  * `:entries`: a list of `Entry` structs in sorted order
+  * `:extensions`: a list of `Extension` structs (not yet implemented)
+  """
+  @type t :: %__MODULE__{
+          version: version,
+          entry_count: non_neg_integer,
+          entries: [__MODULE__.Entry.t()]
+          # extensions: [Extention.t()]
+        }
+
+  @enforce_keys [:version, :entry_count, :entries]
+  defstruct [:version, :entry_count, :entries]
+
+  defmodule Entry do
+    @moduledoc ~S"""
+    A single file (or stage of a file) in an index file.
+
+    An entry represents exactly one stage of a file. If a file path is unmerged
+    then multiple instances may appear for the same path name.
+    """
+
+    alias Xgit.Core.FileMode
+    alias Xgit.Core.ObjectId
+
+    @typedoc ~S"""
+    A single file (or stage of a file) in an index file.
+
+    An entry represents exactly one stage of a file. If a file path is unmerged
+    then multiple instances may appear for the same path name.
+
+    Consult the [documentation for git index file format](https://github.com/git/git/blob/master/Documentation/technical/index-format.txt)
+    for a more detailed description of each item.
+
+    ## Struct Members
+
+    * `name`: entry path name, relative to top-level directory (without leading slash)
+    * `stage`: 0..3 merge status
+    * `object_id`: (ObjectId.t) SHA-1 for the represented object
+    * `mode`: (FileMode.t)
+    * `size`: (integer) on-disk size, possibly truncated to 32 bits
+    * `ctime`: (integer) the last time the file's metadata changed
+    * `ctime_ns`: (integer) nanosecond fraction of `ctime` (if available)
+    * `mtime`: (integer) the last time a file's contents changed
+    * `mtime_ns`: (integer) nanosecond fractino of `mtime` (if available)
+    * `dev`: (integer)
+    * `ino`: (integer)
+    * `uid`: (integer)
+    * `gid`: (integer)
+    * `assume_valid?`: (boolean)
+    * `extended?`: (boolean)
+    * `skip_worktree?`: (boolean)
+    * `intent_to_add?`: (boolean)
+    """
+    @type t :: %__MODULE__{
+            name: charlist,
+            stage: 0..3,
+            object_id: ObjectId.t(),
+            mode: FileMode.t(),
+            size: non_neg_integer,
+            ctime: integer,
+            ctime_ns: non_neg_integer,
+            mtime: integer,
+            mtime_ns: non_neg_integer,
+            dev: integer,
+            ino: integer,
+            uid: integer,
+            gid: integer,
+            assume_valid?: boolean,
+            extended?: boolean,
+            skip_worktree?: boolean,
+            intent_to_add?: boolean
+          }
+
+    @enforce_keys [:name, :stage, :object_id, :size, :mode, :ctime, :mtime]
+
+    defstruct [
+      :name,
+      :stage,
+      :object_id,
+      :size,
+      :mode,
+      :ctime,
+      :mtime,
+      ctime_ns: 0,
+      mtime_ns: 0,
+      dev: 0,
+      ino: 0,
+      uid: 0,
+      gid: 0,
+      assume_valid?: false,
+      extended?: false,
+      skip_worktree?: false,
+      intent_to_add?: false
+    ]
+  end
+
+  @typedoc ~S"""
+  Error codes which can be returned by `from_iodevice/1`.
+  """
+  @type from_iodevice_reason :: :invalid_format | :unsupported_version
+
+  @doc ~S"""
+  Read index file from an `IO.device` (typically an opened file) and returns a
+  corresponding `IndexFile` struct.
+
+  ## Return Value
+
+  `{:ok, index_file}` if the iodevice contains a valid index file.
+
+  `{:error, :invalid_format}` if the iodevice can not be parsed as an index file.
+
+  `{:error, :unsupported_version}` if the index file is not a version 2 index file.
+  Other versions are not supported at this time.
+
+  `{:error, :too_many_entries}` if the index files contains more than 100,000
+  entries. This is an arbitrary limit to guard against malformed files and to
+  prevent overconsumption of memory. With experience, it could be revisited.
+  """
+  @spec from_iodevice(iodevice :: IO.device()) ::
+          {:ok, index_file :: t} | {:error, reason :: from_iodevice_reason}
+  def from_iodevice(iodevice) do
+    with {:dirc, true} <- {:dirc, read_dirc(iodevice)},
+         {:version, version = 2} <- {:version, read_uint32(iodevice)},
+         {:entry_count, entry_count}
+         when is_integer(entry_count) and entry_count <= 100_000 <-
+           {:entry_count, read_uint32(iodevice)},
+         {:entries, entries} when is_list(entries) <-
+           {:entries, read_entries(iodevice, version, entry_count)} do
+      # TO DO: Parse extensions and trailing checksum.
+      # https://github.com/elixir-git/xgit/issues/67
+
+      {:ok,
+       %__MODULE__{
+         version: version,
+         entry_count: entry_count,
+         entries: entries
+       }}
+    else
+      {:dirc, _} -> {:error, :invalid_format}
+      {:version, _} -> {:error, :unsupported_version}
+      {:entry_count, :invalid} -> {:error, :invalid_format}
+      {:entry_count, _} -> {:error, :too_many_entries}
+      {:entries, _} -> {:error, :invalid_format}
+    end
+  end
+
+  defp read_dirc(iodevice) do
+    case IO.binread(iodevice, 4) do
+      "DIRC" -> true
+      _ -> false
+    end
+  end
+
+  defp read_entries(_iodevice, _version, 0), do: []
+
+  defp read_entries(iodevice, version, entry_count) do
+    entries =
+      Enum.map(1..entry_count, fn i ->
+        read_entry(iodevice, version, i == 1)
+      end)
+
+    if Enum.all?(entries, &valid_entry?/1),
+      do: entries,
+      else: :invalid
+  end
+
+  defp read_entry(iodevice, 2 = _version, first?) do
+    with ctime when is_integer(ctime) <- read_uint32(iodevice),
+         ctime_ns when is_integer(ctime_ns) <- read_uint32(iodevice),
+         mtime when is_integer(mtime) <- read_uint32(iodevice),
+         mtime_ns when is_integer(mtime_ns) <- read_uint32(iodevice),
+         dev when is_integer(dev) <- read_uint32(iodevice),
+         ino when is_integer(ino) <- read_uint32(iodevice),
+         mode when is_integer(mode) <- read_uint32(iodevice),
+         uid when is_integer(uid) <- read_uint32(iodevice),
+         gid when is_integer(gid) <- read_uint32(iodevice),
+         size when is_integer(size) <- read_uint32(iodevice),
+         object_id
+         when is_binary(object_id) and object_id != "0000000000000000000000000000000000000000" <-
+           read_object_id(iodevice),
+         flags when is_integer(flags) and flags > 0 <- read_uint16(iodevice),
+         name when is_list(name) <- read_name(iodevice, flags &&& 0xFFF, first?) do
+      %__MODULE__.Entry{
+        name: name,
+        stage: bsr(flags &&& 0x3000, 12),
+        object_id: object_id,
+        size: size,
+        mode: mode,
+        ctime: ctime,
+        ctime_ns: ctime_ns,
+        mtime: mtime,
+        mtime_ns: mtime_ns,
+        dev: dev,
+        ino: ino,
+        uid: uid,
+        gid: gid,
+        assume_valid?: to_boolean(flags &&& 0x8000),
+        extended?: to_boolean(flags &&& 0x4000),
+        skip_worktree?: false,
+        intent_to_add?: false
+      }
+    else
+      _ -> :invalid
+    end
+  end
+
+  defp valid_entry?(%__MODULE__.Entry{}), do: true
+  defp valid_entry?(_), do: false
+
+  defp read_uint16(iodevice) do
+    case IO.binread(iodevice, 2) do
+      x when is_binary(x) and byte_size(x) == 2 ->
+        x
+        |> :binary.bin_to_list()
+        |> NB.decode_uint16()
+        |> elem(0)
+
+      _ ->
+        :invalid
+    end
+  end
+
+  defp read_uint32(iodevice) do
+    case IO.binread(iodevice, 4) do
+      x when is_binary(x) and byte_size(x) == 4 ->
+        x
+        |> :binary.bin_to_list()
+        |> NB.decode_uint32()
+        |> elem(0)
+
+      _ ->
+        :invalid
+    end
+  end
+
+  defp read_object_id(iodevice) do
+    case IO.binread(iodevice, 20) do
+      x when is_binary(x) and byte_size(x) == 20 ->
+        x
+        |> Base.encode16()
+        |> String.downcase()
+
+      _ ->
+        :invalid
+    end
+  end
+
+  defp read_name(iodevice, length, first?) when length < 0xFFF do
+    first_shift = if first?, do: 4, else: 0
+    bytes_to_read = length + padding(Integer.mod(length + first_shift, 8))
+
+    case IO.binread(iodevice, bytes_to_read) do
+      x when is_binary(x) and byte_size(x) == bytes_to_read ->
+        x
+        |> :binary.bin_to_list()
+        |> Enum.take_while(&(&1 != 0))
+
+      _ ->
+        :invalid
+    end
+  end
+
+  defp read_name(_iodevice, _length, _first?), do: :invalid
+
+  defp padding(length_mod_8) when length_mod_8 < 6, do: 6 - length_mod_8
+  defp padding(6), do: 8
+  defp padding(7), do: 7
+
+  defp to_boolean(0), do: false
+  defp to_boolean(_), do: true
+end
---
+++ test/xgit/core/index_file_test.exs
@@ -0,0 +1,238 @@
+defmodule Xgit.Core.IndexFileTest do
+  use Xgit.GitInitTestCase, async: true
+
+  alias Xgit.Core.IndexFile
+
+  describe "from_iodevice/1" do
+    test "happy path: can read from command-line git (empty index)", %{ref: ref} do
+      {_output, 0} =
+        System.cmd(
+          "git",
+          [
+            "update-index",
+            "--add",
+            "--cacheinfo",
+            "100644",
+            "18832d35117ef2f013c4009f5b2128dfaeff354f",
+            "hello.txt"
+          ],
+          cd: ref
+        )
+
+      {_output, 0} =
+        System.cmd(
+          "git",
+          [
+            "update-index",
+            "--remove",
+            "test_content.txt"
+          ],
+          cd: ref
+        )
+
+      assert {:ok, index_file} =
+               [ref, ".git", "index"]
+               |> Path.join()
+               |> File.open!()
+               |> IndexFile.from_iodevice()
+
+      assert index_file = %IndexFile{
+               entries: [],
+               entry_count: 0,
+               version: 2
+             }
+    end
+
+    test "happy path: can read from command-line git (two small files)", %{ref: ref} do
+      {_output, 0} =
+        System.cmd(
+          "git",
+          [
+            "update-index",
+            "--add",
+            "--cacheinfo",
+            "100644",
+            "18832d35117ef2f013c4009f5b2128dfaeff354f",
+            "hello.txt"
+          ],
+          cd: ref
+        )
+
+      {_output, 0} =
+        System.cmd(
+          "git",
+          [
+            "update-index",
+            "--add",
+            "--cacheinfo",
+            "100644",
+            "d670460b4b4aece5915caf5c68d12f560a9fe3e4",
+            "test_content.txt"
+          ],
+          cd: ref
+        )
+
+      assert {:ok, index_file} =
+               [ref, ".git", "index"]
+               |> Path.join()
+               |> File.open!()
+               |> IndexFile.from_iodevice()
+
+      assert index_file = %IndexFile{
+               entries: [
+                 %IndexFile.Entry{
+                   assume_valid?: false,
+                   ctime: 0,
+                   ctime_ns: 0,
+                   dev: 0,
+                   extended?: false,
+                   gid: 0,
+                   ino: 0,
+                   intent_to_add?: false,
+                   mode: 0o100644,
+                   mtime: 0,
+                   mtime_ns: 0,
+                   name: 'hello.txt',
+                   object_id: "18832d35117ef2f013c4009f5b2128dfaeff354f",
+                   size: 0,
+                   skip_worktree?: false,
+                   stage: 0,
+                   uid: 0
+                 },
+                 %IndexFile.Entry{
+                   assume_valid?: false,
+                   ctime: 0,
+                   ctime_ns: 0,
+                   dev: 0,
+                   extended?: false,
+                   gid: 0,
+                   ino: 0,
+                   intent_to_add?: false,
+                   mode: 0o100644,
+                   mtime: 0,
+                   mtime_ns: 0,
+                   name: 'test_content.txt',
+                   object_id: "d670460b4b4aece5915caf5c68d12f560a9fe3e4",
+                   size: 0,
+                   skip_worktree?: false,
+                   stage: 0,
+                   uid: 0
+                 }
+               ],
+               entry_count: 2,
+               version: 2
+             }
+    end
+  end
+
+  test "error: file doesn't start with DIRC signature" do
+    assert {:error, :invalid_format} = parse_iodata_as_index_file('DIRX')
+  end
+
+  test "error: unsupported version" do
+    assert {:error, :unsupported_version} = parse_iodata_as_index_file(['DIRC', 0, 0, 0, 1])
+  end
+
+  test "error: incomplete # entries" do
+    assert {:error, :invalid_format} = parse_iodata_as_index_file(['DIRC', 0, 0, 0, 2, 0, 0])
+  end
+
+  test "error: too many entries" do
+    assert {:error, :too_many_entries} =
+             parse_iodata_as_index_file(['DIRC', 0, 0, 0, 2, 1, 1, 1, 1])
+  end
+
+  test "error: missing entries" do
+    assert {:error, :invalid_format} = parse_iodata_as_index_file(v2_header_with_n_entries(1))
+  end
+
+  test "error: partial entry" do
+    assert {:error, :invalid_format} =
+             parse_iodata_as_index_file([v2_header_with_n_entries(1), 0, 0])
+  end
+
+  test "error: invalid file mode" do
+    # 129, 165 = 0o100645 (invalid)
+    assert {:error, :invalid_format} =
+             parse_iodata_as_index_file([v2_header_with_valid_entry_through_ino(), 0, 0, 129, 165])
+  end
+
+  test "error: missing object ID" do
+    assert {:error, :invalid_format} =
+             parse_iodata_as_index_file(v2_header_with_valid_entry_through_file_size())
+  end
+
+  test "error: partial object ID" do
+    assert {:error, :invalid_format} =
+             parse_iodata_as_index_file([
+               v2_header_with_valid_entry_through_file_size(),
+               [0, 1, 2, 3, 4]
+             ])
+  end
+
+  test "error: null object ID" do
+    assert {:error, :invalid_format} =
+             parse_iodata_as_index_file([
+               v2_header_with_valid_entry_through_file_size(),
+               Enum.map(1..20, fn _ -> 0 end),
+               [0, 9],
+               'hello.txt',
+               0
+             ])
+  end
+
+  test "error: incomplete name" do
+    assert {:error, :invalid_format} =
+             parse_iodata_as_index_file([
+               v2_header_with_valid_entry_through_file_size(),
+               Enum.map(1..20, fn n -> n end),
+               [0, 9],
+               'hello'
+             ])
+  end
+
+  test "error: name missing trailing null" do
+    assert {:error, :invalid_format} =
+             parse_iodata_as_index_file([
+               v2_header_with_valid_entry_through_file_size(),
+               Enum.map(1..20, fn n -> n end),
+               [0, 9],
+               'hello.txt'
+             ])
+  end
+
+  test "error: name too long" do
+    assert {:error, :invalid_format} =
+             parse_iodata_as_index_file([
+               v2_header_with_valid_entry_through_file_size(),
+               Enum.map(1..20, fn n -> n end),
+               [15, 255],
+               'hello.txt'
+             ])
+  end
+
+  defp parse_iodata_as_index_file(iodata) do
+    iodata
+    |> IO.iodata_to_binary()
+    |> stringio_open!()
+    |> IndexFile.from_iodevice()
+  end
+
+  defp stringio_open!(s) do
+    {:ok, iodevice} = StringIO.open(s)
+    iodevice
+  end
+
+  defp v2_header_with_n_entries(n) when n > 0 and n <= 255, do: ['DIRC', 0, 0, 0, 2, 0, 0, 0, n]
+
+  defp v2_header_with_valid_entry_through_ino,
+    do: [v2_header_with_n_entries(1), Enum.map(1..24, fn _ -> 0 end)]
+
+  defp v2_header_with_valid_entry_through_file_size,
+    do: [
+      v2_header_with_valid_entry_through_ino(),
+      [0, 0, 129, 164],
+      Enum.map(1..11, fn _ -> 0 end),
+      14
+    ]
+end
