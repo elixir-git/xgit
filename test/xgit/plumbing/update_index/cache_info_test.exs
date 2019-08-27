a53233a5b9996080a6c16bd06ee45444325b2737
Eric Scouten <eric@scouten.com>
2019-08-26 22:10:05 -0700

    Implement Xgit.Plumbing.UpdateIndex.CacheInfo.
    Analogous to the `--cacheinfo` form of `git update-index`.

---
+++ test/xgit/plumbing/update_index/cache_info_test.exs
@@ -0,0 +1,251 @@
+defmodule Xgit.Plumbing.UpdateInfo.CacheInfoTest do
+  use Xgit.GitInitTestCase, async: true
+
+  alias Xgit.Plumbing.UpdateIndex.CacheInfo
+  alias Xgit.Repository.InMemory
+  alias Xgit.Repository.OnDisk
+
+  import FolderDiff
+
+  describe "run/2" do
+    test "happy path: write to repo matches command-line git (one file)", %{ref: ref, xgit: xgit} do
+      {_output, 0} =
+        System.cmd(
+          "git",
+          [
+            "update-index",
+            "--add",
+            "--cacheinfo",
+            "100644",
+            "18832d35117ef2f013c4009f5b2128dfaeff354f",
+            "hello.txt"
+          ],
+          cd: ref
+        )
+
+      assert :ok = OnDisk.create(xgit)
+      assert {:ok, repo} = OnDisk.start_link(work_dir: xgit)
+
+      assert :ok =
+               CacheInfo.run(
+                 repo,
+                 [{0o100644, "18832d35117ef2f013c4009f5b2128dfaeff354f", 'hello.txt'}]
+               )
+
+      assert_folders_are_equal(ref, xgit)
+    end
+
+    test "happy path: git ls-files can read output from UpdateIndex.CacheInfo (one file)", %{
+      ref: ref
+    } do
+      {:ok, repo} = OnDisk.start_link(work_dir: ref)
+
+      assert :ok =
+               CacheInfo.run(
+                 repo,
+                 [{0o100644, "18832d35117ef2f013c4009f5b2128dfaeff354f", 'hello.txt'}]
+               )
+
+      assert {output, 0} = System.cmd("git", ["ls-files", "--stage"], cd: ref)
+      assert output == "100644 18832d35117ef2f013c4009f5b2128dfaeff354f 0	hello.txt\n"
+    end
+
+    test "happy path: can generate correct empty index file",
+         %{ref: ref, xgit: xgit} do
+      # An initialized git repo doesn't have an index file at all.
+      # Adding and removing a file generates an empty index file.
+
+      {_output, 0} =
+        System.cmd(
+          "git",
+          [
+            "update-index",
+            "--add",
+            "--cacheinfo",
+            "100644",
+            "18832d35117ef2f013c4009f5b2128dfaeff354f",
+            "hello.txt"
+          ],
+          cd: ref
+        )
+
+      {_output, 0} =
+        System.cmd(
+          "git",
+          [
+            "update-index",
+            "--remove",
+            "hello.txt"
+          ],
+          cd: ref
+        )
+
+      assert :ok = OnDisk.create(xgit)
+      {:ok, repo} = OnDisk.start_link(work_dir: xgit)
+
+      assert :ok = CacheInfo.run(repo, [])
+
+      assert_folders_are_equal(ref, xgit)
+    end
+
+    test "can write an index file with entries that matches command-line git",
+         %{ref: ref, xgit: xgit} do
+      {_output, 0} =
+        System.cmd(
+          "git",
+          [
+            "update-index",
+            "--add",
+            "--cacheinfo",
+            "100644",
+            "18832d35117ef2f013c4009f5b2128dfaeff354f",
+            "hello.txt"
+          ],
+          cd: ref
+        )
+
+      {_output, 0} =
+        System.cmd(
+          "git",
+          [
+            "update-index",
+            "--add",
+            "--cacheinfo",
+            "100644",
+            "d670460b4b4aece5915caf5c68d12f560a9fe3e4",
+            "test_content.txt"
+          ],
+          cd: ref
+        )
+
+      assert :ok = OnDisk.create(xgit)
+      {:ok, repo} = OnDisk.start_link(work_dir: xgit)
+
+      assert :ok =
+               CacheInfo.run(
+                 repo,
+                 [
+                   {0o100644, "18832d35117ef2f013c4009f5b2128dfaeff354f", 'hello.txt'},
+                   {0o100644, "d670460b4b4aece5915caf5c68d12f560a9fe3e4", 'test_content.txt'}
+                 ]
+               )
+
+      assert_folders_are_equal(ref, xgit)
+    end
+
+    test "can remove entries from index file",
+         %{ref: ref, xgit: xgit} do
+      {_output, 0} =
+        System.cmd(
+          "git",
+          [
+            "update-index",
+            "--add",
+            "--cacheinfo",
+            "100644",
+            "18832d35117ef2f013c4009f5b2128dfaeff354f",
+            "hello.txt"
+          ],
+          cd: ref
+        )
+
+      assert :ok = OnDisk.create(xgit)
+
+      # For variety, let's use command-line git to write the first index file
+      # and then update it with Xgit.
+
+      {_output, 0} =
+        System.cmd(
+          "git",
+          [
+            "update-index",
+            "--add",
+            "--cacheinfo",
+            "100644",
+            "18832d35117ef2f013c4009f5b2128dfaeff354f",
+            "hello.txt"
+          ],
+          cd: xgit
+        )
+
+      {_output, 0} =
+        System.cmd(
+          "git",
+          [
+            "update-index",
+            "--add",
+            "--cacheinfo",
+            "100644",
+            "d670460b4b4aece5915caf5c68d12f560a9fe3e4",
+            "test_content.txt"
+          ],
+          cd: xgit
+        )
+
+      {:ok, repo} = OnDisk.start_link(work_dir: xgit)
+
+      assert :ok = CacheInfo.run(repo, [], ['test_content.txt'])
+
+      assert_folders_are_equal(ref, xgit)
+    end
+
+    test "error: file doesn't start with DIRC signature", %{xgit: xgit} do
+      git_dir = Path.join(xgit, '.git')
+      File.mkdir_p!(git_dir)
+
+      index = Path.join(git_dir, 'index')
+      File.write!(index, 'DIRX12345678901234567890')
+
+      {:ok, repo} = OnDisk.start_link(work_dir: xgit)
+
+      assert {:error, :invalid_format} = CacheInfo.run(repo, [], ['test_content.txt'])
+    end
+
+    test "error: repository invalid (not PID)" do
+      assert_raise FunctionClauseError, fn ->
+        CacheInfo.run("xgit repo", [])
+      end
+    end
+
+    test "error: repository invalid (PID, but not repo)" do
+      {:ok, not_repo} = GenServer.start_link(NotValid, nil)
+      assert {:error, :invalid_repository} = CacheInfo.run(not_repo, [])
+    end
+
+    test "error: no working tree" do
+      {:ok, repo} = InMemory.start_link()
+      assert {:error, :bare} = CacheInfo.run(repo, [])
+    end
+
+    test "error: invalid index file", %{xgit: xgit} do
+      git_dir = Path.join(xgit, ".git")
+      File.mkdir_p!(git_dir)
+
+      index_path = Path.join(git_dir, "index")
+      File.write!(index_path, "DIRX")
+
+      {:ok, repo} = OnDisk.start_link(work_dir: xgit)
+
+      assert {:error, :invalid_format} = CacheInfo.run(repo, [])
+    end
+
+    test "error: invalid entries (add)", %{xgit: xgit} do
+      assert :ok = OnDisk.create(xgit)
+      {:ok, repo} = OnDisk.start_link(work_dir: xgit)
+
+      assert {:error, :invalid_entry} =
+               CacheInfo.run(repo, [
+                 {0o100644, "18832d35117ef2f013c4009f5b2128dfaeff354f", "hello.txt"}
+               ])
+
+      # Used a string for path here; path should be binary.
+    end
+
+    test "error: invalid entries (remove)", %{xgit: xgit} do
+      assert :ok = OnDisk.create(xgit)
+      {:ok, repo} = OnDisk.start_link(work_dir: xgit)
+
+      assert {:error, :invalid_entry} = CacheInfo.run(repo, [], ["should be a charlist"])
+    end
+  end
+end
