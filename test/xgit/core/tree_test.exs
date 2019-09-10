98db06a7d90d54a70a1c78af44e87283ff448131
Eric Scouten <eric@scouten.com>
2019-09-10 07:36:59 -0700

    Implement Xgit.Core.Tree.from_object/1. (#164)
    * Implement Xgit.Core.Tree.from_object/1.
    
    * Test-internal function shouldn't be marked public.
    
    * Cover not-a-tree case.
    
    * More test coverage.
    
    * More test coverage.
    
    * More coverage.
    
    * Last bit of coverage?
    
    *  Cover invalid file mode case.

--- test/xgit/core/tree_test.exs
+++ test/xgit/core/tree_test.exs
@@ -68,6 +68,169 @@ defmodule Xgit.Core.TreeTest do
     end
   end
 
+  describe "from_object/1" do
+    setup do
+      Temp.track!()
+      repo = Temp.mkdir!()
+
+      {_output, 0} = System.cmd("git", ["init"], cd: repo)
+      objects_dir = Path.join([repo, ".git", "objects"])
+
+      {:ok, xgit} = OnDisk.start_link(work_dir: repo)
+
+      {:ok, repo: repo, objects_dir: objects_dir, xgit: xgit}
+    end
+
+    defp write_git_tree_and_read_xgit_tree_entries(repo, xgit) do
+      {output, 0} = System.cmd("git", ["write-tree", "--missing-ok"], cd: repo)
+      tree_id = String.trim(output)
+
+      assert {:ok, %Object{} = object} = Repository.get_object(xgit, tree_id)
+      assert {:ok, %Tree{entries: entries} = _tree} = Tree.from_object(object)
+
+      entries
+    end
+
+    test "empty tree", %{repo: repo, xgit: xgit} do
+      assert write_git_tree_and_read_xgit_tree_entries(repo, xgit) == []
+    end
+
+    test "tree with one entry", %{repo: repo, xgit: xgit} do
+      {_output, 0} =
+        System.cmd(
+          "git",
+          [
+            "update-index",
+            "--add",
+            "--cacheinfo",
+            "100644",
+            "18832d35117ef2f013c4009f5b2128dfaeff354f",
+            "hello.txt"
+          ],
+          cd: repo
+        )
+
+      assert write_git_tree_and_read_xgit_tree_entries(repo, xgit) == [
+               %Entry{
+                 name: 'hello.txt',
+                 object_id: "18832d35117ef2f013c4009f5b2128dfaeff354f",
+                 mode: 0o100644
+               }
+             ]
+    end
+
+    test "tree with multiple entries", %{repo: repo, xgit: xgit} do
+      {_output, 0} =
+        System.cmd(
+          "git",
+          [
+            "update-index",
+            "--add",
+            "--cacheinfo",
+            "100644",
+            "18832d35117ef2f013c4009f5b2128dfaeff354f",
+            "hello.txt"
+          ],
+          cd: repo
+        )
+
+      {_output, 0} =
+        System.cmd(
+          "git",
+          [
+            "update-index",
+            "--add",
+            "--cacheinfo",
+            "100755",
+            "d670460b4b4aece5915caf5c68d12f560a9fe3e4",
+            "test_content.txt"
+          ],
+          cd: repo
+        )
+
+      assert write_git_tree_and_read_xgit_tree_entries(repo, xgit) == [
+               %Entry{
+                 name: 'hello.txt',
+                 object_id: "18832d35117ef2f013c4009f5b2128dfaeff354f",
+                 mode: 0o100644
+               },
+               %Entry{
+                 name: 'test_content.txt',
+                 object_id: "d670460b4b4aece5915caf5c68d12f560a9fe3e4",
+                 mode: 0o100755
+               }
+             ]
+    end
+
+    test "object is not a tree" do
+      object = %Object{
+        type: :blob,
+        content: 'test content\n',
+        size: 13,
+        id: "d670460b4b4aece5915caf5c68d12f560a9fe3e4"
+      }
+
+      assert {:error, :not_a_tree} = Tree.from_object(object)
+    end
+
+    test "object is an invalid tree (ends after file mode)" do
+      object = %Object{
+        type: :tree,
+        size: 42,
+        id: "d670460b4b4aece5915caf5c68d12f560a9fe3e4",
+        content: '100644'
+      }
+
+      assert {:error, :invalid_format} = Tree.from_object(object)
+    end
+
+    test "object is an invalid tree (invalid file mode)" do
+      object = %Object{
+        type: :tree,
+        size: 42,
+        id: "d670460b4b4aece5915caf5c68d12f560a9fe3e4",
+        content: '100648 A 12345678901234567890'
+      }
+
+      assert {:error, :invalid_format} = Tree.from_object(object)
+    end
+
+    test "object is an invalid tree (invalid file mode, leading 0)" do
+      object = %Object{
+        type: :tree,
+        size: 42,
+        id: "d670460b4b4aece5915caf5c68d12f560a9fe3e4",
+        content: '0100644 A 12345678901234567890'
+      }
+
+      assert {:error, :invalid_format} = Tree.from_object(object)
+    end
+
+    test "object is an invalid tree (not properly sorted)" do
+      object = %Object{
+        type: :tree,
+        size: 42,
+        id: "d670460b4b4aece5915caf5c68d12f560a9fe3e4",
+        content:
+          '100644 B' ++
+            Enum.map(0..20, fn x -> x end) ++ '100644 A' ++ Enum.map(0..20, fn x -> x end)
+      }
+
+      assert {:error, :invalid_tree} = Tree.from_object(object)
+    end
+
+    test "object is a badly-formatted tree" do
+      object = %Object{
+        type: :tree,
+        size: 42,
+        id: "d670460b4b4aece5915caf5c68d12f560a9fe3e4",
+        content: '100644 A' ++ Enum.map(0..20, fn _ -> 0 end)
+      }
+
+      assert {:error, :invalid_format} = Tree.from_object(object)
+    end
+  end
+
   describe "to_object/1" do
     test "empty tree" do
       assert_same_output(
