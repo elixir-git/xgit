6f05df3ac1caa0877f7d15c7aa2f67cfcccbbd20
Eric Scouten <eric@scouten.com>
2019-09-25 19:56:42 -0700

    Add missing_ok? tests.

--- test/xgit/repository/working_tree/read_tree_test.exs
+++ test/xgit/repository/working_tree/read_tree_test.exs
@@ -4,6 +4,7 @@ defmodule Xgit.Repository.WorkingTree.ReadTreeTest do
   alias Xgit.Core.DirCache
   alias Xgit.Core.DirCache.Entry
   alias Xgit.GitInitTestCase
+  alias Xgit.Plumbing.UpdateIndex.CacheInfo
   # alias Xgit.Plumbing.HashObject
   alias Xgit.Repository
   alias Xgit.Repository.OnDisk
@@ -311,7 +312,90 @@ defmodule Xgit.Repository.WorkingTree.ReadTreeTest do
              }
     end
 
-    # TO DO: Bring back missing_ok? tests
+    test "missing_ok?: false happy path" do
+      Temp.track!()
+      path = Temp.path!()
+      File.write!(path, "test content\n")
+
+      assert write_git_tree_and_read_back(
+               fn git_dir ->
+                 {output, 0} = System.cmd("git", ["hash-object", "-w", path], cd: git_dir)
+                 object_id = String.trim(output)
+
+                 {_output, 0} =
+                   System.cmd(
+                     "git",
+                     ["update-index", "--add", "--cacheinfo", "100644", object_id, "a/b"],
+                     cd: git_dir
+                   )
+               end,
+               missing_ok?: false
+             ) == %DirCache{
+               entries: [
+                 %DirCache.Entry{
+                   assume_valid?: false,
+                   ctime: 0,
+                   ctime_ns: 0,
+                   dev: 0,
+                   extended?: false,
+                   gid: 0,
+                   ino: 0,
+                   intent_to_add?: false,
+                   mode: 33188,
+                   mtime: 0,
+                   mtime_ns: 0,
+                   name: 'a/b',
+                   object_id: "d670460b4b4aece5915caf5c68d12f560a9fe3e4",
+                   size: 0,
+                   skip_worktree?: false,
+                   stage: 0,
+                   uid: 0
+                 }
+               ],
+               entry_count: 1,
+               version: 2
+             }
+    end
+
+    test "missing_ok? error" do
+      {:ok, ref: _ref, xgit: xgit} = GitInitTestCase.setup_git_repo()
+
+      :ok = OnDisk.create(xgit)
+      {:ok, repo} = OnDisk.start_link(work_dir: xgit)
+
+      :ok
+
+      CacheInfo.run(
+        repo,
+        [{0o100644, "7919e8900c3af541535472aebd56d44222b7b3a3", 'hello.txt'}]
+      )
+
+      {output, 0} = System.cmd("git", ["write-tree", "--missing-ok"], cd: xgit)
+      tree_object_id = String.trim(output)
+
+      working_tree = Repository.default_working_tree(repo)
+
+      assert {:error, :objects_missing} = WorkingTree.read_tree(working_tree, tree_object_id)
+    end
+
+    test "error: :missing_ok? invalid" do
+      {:ok, ref: _ref, xgit: xgit} = GitInitTestCase.setup_git_repo()
+
+      :ok = OnDisk.create(xgit)
+      {:ok, repo} = OnDisk.start_link(work_dir: xgit)
+
+      working_tree = Repository.default_working_tree(repo)
+
+      assert_raise ArgumentError,
+                   ~s(Xgit.Repository.WorkingTree.read_tree/3: missing_ok? "sure" is invalid),
+                   fn ->
+                     WorkingTree.read_tree(
+                       working_tree,
+                       "7919e8900c3af541535472aebd56d44222b7b3a3",
+                       missing_ok?: "sure"
+                     )
+                   end
+    end
 
     # test "error: can't write tree object" do
     #   {:ok, ref: _ref, xgit: xgit} = GitInitTestCase.setup_git_repo()
