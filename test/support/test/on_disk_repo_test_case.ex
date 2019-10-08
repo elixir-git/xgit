addfd3f776a92c5a0788ce147ca2dc2f7a394a16
Eric Scouten <eric@scouten.com>
2019-10-07 20:13:43 -0700

    OnDiskRepoTestCase: Make it possible to override the "temporary" directory for testing purposes.

--- test/support/test/on_disk_repo_test_case.ex
+++ test/support/test/on_disk_repo_test_case.ex
@@ -14,10 +14,30 @@ defmodule Xgit.Test.OnDiskRepoTestCase do
   @doc ~S"""
   Returns a context with an on-disk repository set up.
   """
-  @spec repo!() :: %{tmp_dir: Path.t(), xgit_path: Path.t(), xgit_repo: Repository.t()}
-  def repo! do
-    %{tmp_dir: xgit_path} = context = TempDirTestCase.tmp_dir!()
+  @spec repo! :: %{tmp_dir: Path.t(), xgit_path: Path.t(), xgit_repo: Repository.t()}
+  def repo!, do: git_init_repo(TempDirTestCase.tmp_dir!())
 
+  @doc ~S"""
+  Returns a context with an on-disk repository set up.
+
+  Unlike `repo!/0`, this takes a hard-wired path and uses that instead.
+  Ensures that this directory is created and empty.
+
+  Use this when debugging tests that fail so you can inspect contents
+  after the test run.
+  """
+  @spec repo!(dir :: Path.t()) :: %{
+          tmp_dir: Path.t(),
+          xgit_path: Path.t(),
+          xgit_repo: Repository.t()
+        }
+  def repo!(dir) when is_binary(dir) do
+    File.rm_rf!(dir)
+    File.mkdir!(dir)
+    git_init_repo(%{tmp_dir: dir})
+  end
+
+  defp git_init_repo(%{tmp_dir: xgit_path} = context) do
     {_output, 0} = System.cmd("git", ["init"], cd: xgit_path)
     {:ok, xgit_repo} = OnDisk.start_link(work_dir: xgit_path)
 
