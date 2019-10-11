a2ba7c9d763e384992390f11f7cc01c7e00e8c0c
Eric Scouten <eric@scouten.com>
2019-10-11 11:19:00 -0700

    Remove RawParseUtils.parse_hex_* functions.
    Not used anywhere.

--- test/xgit/util/raw_parse_utils_test.exs
+++ test/xgit/util/raw_parse_utils_test.exs
@@ -96,27 +96,6 @@ defmodule Xgit.Util.RawParseUtilsTest do
     assert RPU.parse_base_10('   xyz') == {0, 'xyz'}
   end
 
-  test "parse_hex_int16/1" do
-    assert RPU.parse_hex_int16('00FFasd') == {255, 'asd'}
-    assert RPU.parse_hex_int16('03FF') == {1023, ''}
-    assert RPU.parse_hex_int16('03ff') == {1023, ''}
-  end
-
-  test "parse_hex_int32/1" do
-    assert RPU.parse_hex_int32('80000001xzx') == {2_147_483_649, 'xzx'}
-    assert RPU.parse_hex_int32('000003Ff') == {1023, ''}
-  end
-
-  test "parse_hex_int64/1" do
-    assert RPU.parse_hex_int64('8000000420000001abc') == {9_223_372_054_571_515_905, 'abc'}
-    assert RPU.parse_hex_int64('E0000004200f0001') == {16_140_901_082_213_580_801, ''}
-  end
-
-  test "parse_hex_int4/1" do
-    assert RPU.parse_hex_int4('84') == {8, '4'}
-    assert RPU.parse_hex_int4('E') == {14, ''}
-  end
-
   test "parse_timezone_offset/1" do
     assert RPU.parse_timezone_offset('0') == {0, ''}
     assert RPU.parse_timezone_offset('') == {0, ''}
