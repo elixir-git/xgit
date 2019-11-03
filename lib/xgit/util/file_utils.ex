7b2775d010321c1ec89bf12d4d78d6db0098e4fc
Eric Scouten <eric@scouten.com>
2019-11-03 10:34:24 -0800

    Implement Xgit.Util.FileUtils.recursive_files!/1.

---
+++ lib/xgit/util/file_utils.ex
@@ -0,0 +1,27 @@
+defmodule Xgit.Util.FileUtils do
+  @moduledoc false
+
+  # Internal utility for recursively listing the contents of a directory.
+
+  @doc ~S"""
+  Recursively list the files of a directory.
+
+  Directories are scanned, but their paths are not reported as part of the result.
+  """
+  @spec recursive_files!(path :: Path.t()) :: [Path.t()]
+  def recursive_files!(path \\ ".") do
+    cond do
+      File.regular?(path) ->
+        [path]
+
+      File.dir?(path) ->
+        File.ls!(path)
+        |> Enum.map(&Path.join(path, &1))
+        |> Enum.map(&recursive_files!/1)
+        |> Enum.concat()
+
+      true ->
+        []
+    end
+  end
+end
