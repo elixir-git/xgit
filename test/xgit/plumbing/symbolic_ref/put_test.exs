2ffdc8b7ce31913bf0826de112a34e64f064820e
Eric Scouten <eric@scouten.com>
2019-12-05 20:30:58 -0800

    Implement Xgit.Plumbing.SymbolicRef.Put.
    This is an API analogue to the 2-argument form of `git symbolic-ref`.

---
+++ test/xgit/plumbing/symbolic_ref/put_test.exs
@@ -0,0 +1,131 @@
+defmodule Xgit.Plumbing.SymbolicRef.PutTest do
+  use ExUnit.Case, async: true
+
+  # alias Xgit.Core.Object
+  # alias Xgit.Core.ObjectId
+  alias Xgit.Core.Ref
+  alias Xgit.Plumbing.HashObject
+  alias Xgit.Plumbing.SymbolicRef.Put
+  alias Xgit.Plumbing.UpdateRef
+  alias Xgit.Repository
+  alias Xgit.Test.OnDiskRepoTestCase
+
+  import FolderDiff
+
+  # @env OnDiskRepoTestCase.sample_commit_env()
+
+  describe "run/4" do
+    test "happy path: target ref does not exist" do
+      %{xgit_path: path, xgit_repo: repo} = OnDiskRepoTestCase.repo!()
+
+      assert :ok = Put.run(repo, "HEAD", "refs/heads/nope")
+
+      assert {"refs/heads/nope\n", 0} = System.cmd("git", ["symbolic-ref", "HEAD"], cd: path)
+    end
+
+    # @test_content 'test content\n'
+    # @test_content_id "d670460b4b4aece5915caf5c68d12f560a9fe3e4"
+
+    test "error: posix error (dir where file should be)" do
+      %{xgit_repo: repo, xgit_path: xgit_path} = OnDiskRepoTestCase.repo!()
+
+      File.mkdir_p!(Path.join(xgit_path, ".git/refs/heads/whatever"))
+
+      assert {:error, :eisdir} = Put.run(repo, "refs/heads/whatever", "refs/heads/master")
+    end
+
+    test "error: posix error (file where dir should be)" do
+      %{xgit_repo: repo, xgit_path: xgit_path} = OnDiskRepoTestCase.repo!()
+
+      File.write!(Path.join(xgit_path, ".git/refs/heads/sub"), "oops, not a directory")
+
+      assert {:error, :eexist} = Put.run(repo, "refs/heads/sub/master", "refs/heads/master")
+    end
+
+    test "follows HEAD reference after it changes" do
+      %{xgit_repo: repo} = OnDiskRepoTestCase.repo!()
+
+      {:ok, commit_id_master} =
+        HashObject.run('shhh... not really a commit',
+          repo: repo,
+          type: :commit,
+          validate?: false,
+          write?: true
+        )
+
+      master_ref = %Ref{
+        name: "refs/heads/master",
+        target: commit_id_master
+      }
+
+      master_ref_via_head = %Ref{
+        name: "HEAD",
+        target: commit_id_master,
+        link_target: "refs/heads/master"
+      }
+
+      assert :ok = UpdateRef.run(repo, "HEAD", commit_id_master)
+
+      assert {:ok, [^master_ref]} = Repository.list_refs(repo)
+      assert {:ok, ^master_ref} = Repository.get_ref(repo, "refs/heads/master")
+      assert {:ok, ^master_ref_via_head} = Repository.get_ref(repo, "HEAD")
+
+      {:ok, commit_id_other} =
+        HashObject.run('shhh... another not commit',
+          repo: repo,
+          type: :commit,
+          validate?: false,
+          write?: true
+        )
+
+      other_ref = %Ref{
+        name: "refs/heads/other",
+        target: commit_id_other
+      }
+
+      other_ref_via_head = %Ref{
+        name: "HEAD",
+        target: commit_id_other,
+        link_target: "refs/heads/other"
+      }
+
+      assert :ok = Put.run(repo, "HEAD", "refs/heads/other")
+
+      assert :ok = UpdateRef.run(repo, "HEAD", commit_id_other)
+
+      assert {:ok, [^master_ref, ^other_ref]} = Repository.list_refs(repo)
+      assert {:ok, ^master_ref} = Repository.get_ref(repo, "refs/heads/master")
+      assert {:ok, ^other_ref_via_head} = Repository.get_ref(repo, "HEAD")
+    end
+
+    test "result can be read by command-line git" do
+      %{xgit_repo: repo, xgit_path: path} = OnDiskRepoTestCase.repo!()
+
+      assert :ok = Put.run(repo, "HEAD", "refs/heads/other")
+      assert {"refs/heads/other\n", 0} = System.cmd("git", ["symbolic-ref", "HEAD"], cd: path)
+    end
+
+    test "matches command-line output" do
+      %{xgit_path: xgit_path, xgit_repo: xgit_repo} = OnDiskRepoTestCase.repo!()
+      %{xgit_path: ref_path} = OnDiskRepoTestCase.repo!()
+
+      {_, 0} = System.cmd("git", ["symbolic-ref", "HEAD", "refs/heads/other"], cd: ref_path)
+
+      :ok = Put.run(xgit_repo, "HEAD", "refs/heads/other")
+
+      assert_folders_are_equal(ref_path, xgit_path)
+    end
+
+    test "error: repository invalid (not PID)" do
+      assert_raise FunctionClauseError, fn ->
+        Put.run("xgit repo", "HEAD", "refs/heads/master")
+      end
+    end
+
+    test "error: repository invalid (PID, but not repo)" do
+      {:ok, not_repo} = GenServer.start_link(NotValid, nil)
+
+      assert {:error, :invalid_repository} = UpdateRef.run(not_repo, "HEAD", "refs/heads/master")
+    end
+  end
+end
