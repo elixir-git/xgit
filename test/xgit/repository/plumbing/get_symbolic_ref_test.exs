b1bd1a34bb138659c1167a1d9c87bdb92a081053
Eric Scouten <eric@scouten.com>
2019-12-16 19:05:50 -0800

    Implement `Xgit.Repository.Plumbing.get_symbolic_ref/2`.
    This is an API equivalent to the one-argument form of `git symbolic-ref`.

---
+++ test/xgit/repository/plumbing/get_symbolic_ref_test.exs
@@ -0,0 +1,50 @@
+defmodule Xgit.Repository.Plumbing.GetSymbolicRefTest do
+  use ExUnit.Case, async: true
+
+  # alias Xgit.Ref
+  alias Xgit.Repository.Plumbing
+  alias Xgit.Test.OnDiskRepoTestCase
+
+  describe "get_symbolic_ref/2" do
+    test "happy path: default HEAD branch points to master" do
+      %{xgit_repo: repo} = OnDiskRepoTestCase.repo!()
+      assert {:ok, "refs/heads/master"} = Plumbing.get_symbolic_ref(repo, "HEAD")
+    end
+
+    test "happy path: HEAD branch points to non-existent branch" do
+      %{xgit_repo: repo} = OnDiskRepoTestCase.repo!()
+
+      assert :ok = Plumbing.put_symbolic_ref(repo, "HEAD", "refs/heads/nope")
+
+      assert {:ok, "refs/heads/nope"} = Plumbing.get_symbolic_ref(repo, "HEAD")
+    end
+
+    test "error: posix error (dir where file should be)" do
+      %{xgit_repo: repo, xgit_path: xgit_path} = OnDiskRepoTestCase.repo!()
+
+      File.mkdir_p!(Path.join(xgit_path, ".git/refs/heads/whatever"))
+
+      assert {:error, :eisdir} = Plumbing.get_symbolic_ref(repo, "refs/heads/whatever")
+    end
+
+    test "error: posix error (file where dir should be)" do
+      %{xgit_repo: repo, xgit_path: xgit_path} = OnDiskRepoTestCase.repo!()
+
+      File.write!(Path.join(xgit_path, ".git/refs/heads/sub"), "oops, not a directory")
+
+      assert {:error, :not_found} = Plumbing.get_symbolic_ref(repo, "refs/heads/sub/master")
+    end
+
+    test "error: repository invalid (not PID)" do
+      assert_raise FunctionClauseError, fn ->
+        Plumbing.get_symbolic_ref("xgit repo", "HEAD")
+      end
+    end
+
+    test "error: repository invalid (PID, but not repo)" do
+      {:ok, not_repo} = GenServer.start_link(NotValid, nil)
+
+      assert {:error, :invalid_repository} = Plumbing.get_symbolic_ref(not_repo, "HEAD")
+    end
+  end
+end
