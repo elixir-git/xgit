d4d7a65f9c3130f5a54c6c8584d1160425438989
Eric Scouten <eric@scouten.com>
2020-01-25 19:28:05 -0800

    WIP on Xgit.Util.ConfigFile.add_config_entries/3.

--- lib/xgit/util/config_file.ex
+++ lib/xgit/util/config_file.ex
@@ -320,10 +320,61 @@ defmodule Xgit.Util.ConfigFile do
 
   defp empty_config, do: cover([])
 
+  @typedoc ~S"""
+  Error codes that can be returned by `add_config_entries/3`.
+  """
+  @type add_config_entries_reason :: File.posix()
+
+  @doc ~S"""
+  Add one or more new entries to an existing config.
+
+  The entries need not be sorted. However, if multiple values are provided
+  for the same variable (section, subsection, name tuple), they will be added
+  in the order provided here.
+
+  ## Parameters
+
+  `entries` (list of `Xgit.ConfigEntry`) entries to be added
+
+  ## Options
+
+  `add?`: if `true`, adds these entries to any that may already exist
+  `replace_all?`: if `true`, removes all existing entries that match any keys provided
+
+  See also the `:remove_all` option for the `value` member of `Xgit.ConfigEntry`.
+
+  ## Return Values
+
+  `:ok` if successful.
+
+  `{:error, reason}` if unable. `reason` is likely a POSIX error code.
+  """
+  @spec add_config_entries(config_file :: t, entries :: [Xgit.ConfigEntry.t()],
+          add?: boolean,
+          replace_all?: boolean
+        ) ::
+          :ok | {:error, config_file :: add_config_entries_reason}
+  def add_config_entries(config_file, entries, opts \\ [])
+      when is_pid(config_file) and is_list(entries) and is_list(opts) do
+    if Enum.all?(entries, &ConfigEntry.valid?/1) do
+      GenServer.call(config_file, {:add_config_entries, entries, opts})
+    else
+      raise ArgumentError,
+            "Xgit.Util.ConfigFile.add_config_entries/3: one or more entries are invalid"
+    end
+  end
+
+  defp handle_add_config_entries(%ObservedFile{} = _of, _entries, _opts) do
+    raise "unimplemented"
+  end
+
   ## --- Callbacks ---
 
   @impl true
   def handle_call({:get_entries, opts}, _from, state), do: handle_get_entries(state, opts)
+
+  def handle_call({:add_config_entries, entries, opts}, _from, state),
+    do: handle_add_config_entries(state, entries, opts)
 
   def handle_call(message, _from, state) do
     Logger.warn("ConfigFile received unrecognized call #{inspect(message)}")
