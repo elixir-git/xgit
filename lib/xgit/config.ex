246a8da1a2011ff9538e6759f6f71c3b0f472140
Eric Scouten <eric@scouten.com>
2020-03-29 21:18:04 -0700

    Implement Xgit.Config module. (#314)
    * Implement Xgit.Config module, starting with Config.get_string_list/4.
    
    * Fix borked naming.
    
    * Implement Xgit.Config.get_string/4.
    
    * Implement Xgit.Config.get_integer/5.
    
    * Add placeholder reminders to scale with K, M, and G suffixes.
    
    * Add basic case for Xgit.Config.get_boolean/5.
    
    * Check the various aliases for true and false.
    
    * Xgit.Config.get_string/4 returns :empty if single value exists with no =.
    
    * Xgit.Config.get_boolean/5 handles empty string and missing = cases correctly now.
    
    * Add support for integers scaling with K, M, and G suffixes.

---
+++ lib/xgit/config.ex
@@ -0,0 +1,148 @@
+defmodule Xgit.Config do
+  @moduledoc ~S"""
+  Provides convenience functions to get specific configuration values from a repository.
+
+  IMPORTANT: The on-disk repository implementation (`Xgit.Repository.OnDisk`) does not
+  examine configuration directives in global or home directory.
+  """
+
+  alias Xgit.Repository
+  alias Xgit.Repository.Storage
+
+  import Xgit.Util.ForceCoverage
+
+  @doc ~S"""
+  Get the list of strings for this variable.
+  """
+  @spec get_string_list(
+          repository :: Repository.t(),
+          section :: String.t(),
+          subsection :: String.t() | nil,
+          name :: String.t()
+        ) :: [String.t() | nil]
+  def get_string_list(repository, section, subsection \\ nil, name)
+
+  def get_string_list(repository, section, nil, name)
+      when is_binary(section) and is_binary(name) do
+    repository
+    |> Storage.get_config_entries(section: section, name: name)
+    |> entries_to_values()
+  end
+
+  def get_string_list(repository, section, subsection, name)
+      when is_binary(section) and is_binary(subsection) and is_binary(name) do
+    repository
+    |> Storage.get_config_entries(section: section, subsection: subsection, name: name)
+    |> entries_to_values()
+  end
+
+  defp entries_to_values(config_entries) do
+    Enum.map(config_entries, fn %{value: value} ->
+      cover value
+    end)
+  end
+
+  @doc ~S"""
+  If there is a single string for this variable, return it.
+
+  If there are zero or multiple values for this variable, return `nil`.
+
+  If there is exactly one value, but it was implied (missing `=`), return `:empty`.
+  """
+  @spec get_string(
+          repository :: Repository.t(),
+          section :: String.t(),
+          subsection :: String.t() | nil,
+          name :: String.t()
+        ) :: String.t() | nil | :empty
+  def get_string(repository, section, subsection \\ nil, name) do
+    repository
+    |> get_string_list(section, subsection, name)
+    |> single_string_value()
+  end
+
+  defp single_string_value([value]) when is_binary(value) do
+    cover value
+  end
+
+  defp single_string_value([nil]) do
+    cover :empty
+  end
+
+  defp single_string_value(_) do
+    cover nil
+  end
+
+  @doc ~S"""
+  Return the config value interpreted as an integer.
+
+  Use `default` if it can not be interpreted as such.
+  """
+  @spec get_integer(
+          repository :: Repository.t(),
+          section :: String.t(),
+          subsection :: String.t() | nil,
+          name :: String.t(),
+          default :: integer()
+        ) :: integer()
+  def get_integer(repository, section, subsection \\ nil, name, default)
+      when is_integer(default) do
+    repository
+    |> get_string(section, subsection, name)
+    |> to_integer_or_default(default)
+  end
+
+  defp to_integer_or_default(nil, default), do: cover(default)
+  defp to_integer_or_default(:empty, default), do: cover(default)
+
+  defp to_integer_or_default(value, default) do
+    case Integer.parse(value) do
+      {n, ""} -> cover n
+      {n, "k"} -> cover n * 1024
+      {n, "K"} -> cover n * 1024
+      {n, "m"} -> cover n * 1024 * 1024
+      {n, "M"} -> cover n * 1024 * 1024
+      {n, "g"} -> cover n * 1024 * 1024 * 1024
+      {n, "G"} -> cover n * 1024 * 1024 * 1024
+      _ -> cover default
+    end
+  end
+
+  @doc ~S"""
+  Return the config value interpreted as a boolean.
+
+  Use `default` if it can not be interpreted as such.
+  """
+  @spec get_boolean(
+          repository :: Repository.t(),
+          section :: String.t(),
+          subsection :: String.t() | nil,
+          name :: String.t(),
+          default :: boolean()
+        ) :: boolean()
+  def get_boolean(repository, section, subsection \\ nil, name, default)
+      when is_boolean(default) do
+    repository
+    |> get_string(section, subsection, name)
+    |> to_lower_if_string()
+    |> to_boolean_or_default(default)
+  end
+
+  defp to_lower_if_string(nil), do: cover(nil)
+  defp to_lower_if_string(:empty), do: cover(:empty)
+  defp to_lower_if_string(s) when is_binary(s), do: String.downcase(s)
+
+  defp to_boolean_or_default("yes", _default), do: cover(true)
+  defp to_boolean_or_default("on", _default), do: cover(true)
+  defp to_boolean_or_default("true", _default), do: cover(true)
+  defp to_boolean_or_default("1", _default), do: cover(true)
+  defp to_boolean_or_default(:empty, _default), do: cover(true)
+
+  defp to_boolean_or_default("no", _default), do: cover(false)
+  defp to_boolean_or_default("off", _default), do: cover(false)
+  defp to_boolean_or_default("false", _default), do: cover(false)
+  defp to_boolean_or_default("0", _default), do: cover(false)
+  defp to_boolean_or_default("", _default), do: cover(false)
+
+  defp to_boolean_or_default(_, default), do: cover(default)
+end
---
+++ test/xgit/config_test.exs
@@ -0,0 +1,539 @@
+defmodule Xgit.ConfigTest do
+  use ExUnit.Case, async: true
+
+  alias Xgit.Config
+  alias Xgit.ConfigEntry
+  alias Xgit.Repository.InMemory
+  alias Xgit.Repository.Storage
+
+  setup do
+    {:ok, repo} = InMemory.start_link()
+    {:ok, repo: repo}
+  end
+
+  describe "get_string_list/4" do
+    test "empty case (no subsection)", %{repo: repo} do
+      assert [] = Config.get_string_list(repo, "core", "blah")
+    end
+
+    test "empty case (with subsection)", %{repo: repo} do
+      assert [] = Config.get_string_list(repo, "core", "subsection", "blah")
+    end
+
+    test "strings exist (no subsection)", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: nil,
+          name: "blah",
+          value: "foo"
+        })
+
+      :ok =
+        Storage.add_config_entry(
+          repo,
+          %ConfigEntry{
+            section: "test",
+            subsection: nil,
+            name: "blah",
+            value: "foo2"
+          },
+          add?: true
+        )
+
+      assert ["foo", "foo2"] = Config.get_string_list(repo, "test", "blah")
+    end
+
+    test "strings exist (with subsection)", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: "sub",
+          name: "blah",
+          value: "foo"
+        })
+
+      :ok =
+        Storage.add_config_entry(
+          repo,
+          %ConfigEntry{
+            section: "test",
+            subsection: "sub",
+            name: "blah",
+            value: "foo2"
+          },
+          add?: true
+        )
+
+      assert ["foo", "foo2"] = Config.get_string_list(repo, "test", "sub", "blah")
+    end
+  end
+
+  describe "get_string/4" do
+    test "empty case (no subsection)", %{repo: repo} do
+      assert Config.get_string(repo, "core", "blah") == nil
+    end
+
+    test "empty case (with subsection)", %{repo: repo} do
+      assert Config.get_string(repo, "core", "subsection", "blah") == nil
+    end
+
+    test "single string exists (no subsection)", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: nil,
+          name: "blah",
+          value: "foo"
+        })
+
+      assert Config.get_string(repo, "test", "blah") == "foo"
+    end
+
+    test "single string exists (with subsection)", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: "sub",
+          name: "blah",
+          value: "foo"
+        })
+
+      assert Config.get_string(repo, "test", "sub", "blah") == "foo"
+    end
+
+    test "single string exists (no = sign)", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: nil,
+          name: "blah",
+          value: nil
+        })
+
+      assert Config.get_string(repo, "test", "blah") == :empty
+    end
+
+    test "single string exists (no value after =)", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: nil,
+          name: "blah",
+          value: ""
+        })
+
+      assert Config.get_string(repo, "test", "blah") == ""
+    end
+
+    test "multiple strings exist (no subsection)", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: nil,
+          name: "blah",
+          value: "foo"
+        })
+
+      :ok =
+        Storage.add_config_entry(
+          repo,
+          %ConfigEntry{
+            section: "test",
+            subsection: nil,
+            name: "blah",
+            value: "foo2"
+          },
+          add?: true
+        )
+
+      assert Config.get_string(repo, "test", "blah") == nil
+    end
+
+    test "multiple strings exist (with subsection)", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: "sub",
+          name: "blah",
+          value: "foo"
+        })
+
+      :ok =
+        Storage.add_config_entry(
+          repo,
+          %ConfigEntry{
+            section: "test",
+            subsection: "sub",
+            name: "blah",
+            value: "foo2"
+          },
+          add?: true
+        )
+
+      assert Config.get_string(repo, "test", "sub", "blah") == nil
+    end
+  end
+
+  describe "get_integer/5" do
+    test "empty case (no subsection)", %{repo: repo} do
+      assert Config.get_integer(repo, "core", "blah", 42) == 42
+    end
+
+    test "empty case (with subsection)", %{repo: repo} do
+      assert Config.get_integer(repo, "core", "subsection", "blah", 59) == 59
+    end
+
+    test "single integer string exists (no subsection)", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: nil,
+          name: "blah",
+          value: "43"
+        })
+
+      assert Config.get_integer(repo, "test", "blah", 52) == 43
+    end
+
+    test "single integer string exists (with subsection)", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: "sub",
+          name: "blah",
+          value: "-87"
+        })
+
+      assert Config.get_integer(repo, "test", "sub", "blah", 87) == -87
+    end
+
+    test "single empty value exists", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: nil,
+          name: "blah",
+          value: nil
+        })
+
+      assert Config.get_integer(repo, "test", "blah", 52) == 52
+    end
+
+    test "single invalid string exists (no subsection)", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: nil,
+          name: "blah",
+          value: "33.5"
+        })
+
+      assert Config.get_integer(repo, "test", "blah", 56) == 56
+    end
+
+    test "single invalid string exists (with subsection)", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: "sub",
+          name: "blah",
+          value: "foo"
+        })
+
+      assert Config.get_integer(repo, "test", "sub", "blah", 314) == 314
+    end
+
+    test "multiple strings exist (no subsection)", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: nil,
+          name: "blah",
+          value: "14"
+        })
+
+      :ok =
+        Storage.add_config_entry(
+          repo,
+          %ConfigEntry{
+            section: "test",
+            subsection: nil,
+            name: "blah",
+            value: "15"
+          },
+          add?: true
+        )
+
+      assert Config.get_integer(repo, "test", "blah", 13) == 13
+    end
+
+    test "multiple strings exist (with subsection)", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: "sub",
+          name: "blah",
+          value: "22"
+        })
+
+      :ok =
+        Storage.add_config_entry(
+          repo,
+          %ConfigEntry{
+            section: "test",
+            subsection: "sub",
+            name: "blah",
+            value: "24"
+          },
+          add?: true
+        )
+
+      assert Config.get_integer(repo, "test", "sub", "blah", 97) == 97
+    end
+
+    test "scale with k suffix", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: nil,
+          name: "blah",
+          value: "5k"
+        })
+
+      assert Config.get_integer(repo, "test", "blah", 52) == 5120
+    end
+
+    test "scale with K suffix", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: nil,
+          name: "blah",
+          value: "5K"
+        })
+
+      assert Config.get_integer(repo, "test", "blah", 52) == 5120
+    end
+
+    test "scale with m suffix", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: nil,
+          name: "blah",
+          value: "4m"
+        })
+
+      assert Config.get_integer(repo, "test", "blah", 52) == 4_194_304
+    end
+
+    test "scale with M suffix", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: nil,
+          name: "blah",
+          value: "4M"
+        })
+
+      assert Config.get_integer(repo, "test", "blah", 52) == 4_194_304
+    end
+
+    test "scale with g suffix", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: nil,
+          name: "blah",
+          value: "3g"
+        })
+
+      assert Config.get_integer(repo, "test", "blah", 52) == 3_221_225_472
+    end
+
+    test "scale with G suffix", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: nil,
+          name: "blah",
+          value: "3G"
+        })
+
+      assert Config.get_integer(repo, "test", "blah", 52) == 3_221_225_472
+    end
+  end
+
+  describe "get_boolean/5" do
+    test "empty case (no subsection)", %{repo: repo} do
+      assert Config.get_boolean(repo, "core", "blah", true) == true
+    end
+
+    test "empty case (with subsection)", %{repo: repo} do
+      assert Config.get_boolean(repo, "core", "subsection", "blah", false) == false
+    end
+
+    test "single boolean string exists (no subsection)", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: nil,
+          name: "blah",
+          value: "true"
+        })
+
+      assert Config.get_boolean(repo, "test", "blah", false) == true
+    end
+
+    test "single boolean string exists (with subsection)", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: "sub",
+          name: "blah",
+          value: "false"
+        })
+
+      assert Config.get_boolean(repo, "test", "sub", "blah", true) == false
+    end
+
+    test "single invalid string exists (no subsection)", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: nil,
+          name: "blah",
+          value: "33"
+        })
+
+      assert Config.get_boolean(repo, "test", "blah", false) == false
+    end
+
+    test "single invalid string exists (with subsection)", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: "sub",
+          name: "blah",
+          value: "foo"
+        })
+
+      assert Config.get_boolean(repo, "test", "sub", "blah", true) == true
+    end
+
+    defp check_true_alias(repo, value) do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: nil,
+          name: "blah",
+          value: value
+        })
+
+      assert Config.get_boolean(repo, "test", "blah", false) == true
+    end
+
+    defp check_false_alias(repo, value) do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: nil,
+          name: "blah",
+          value: value
+        })
+
+      assert Config.get_boolean(repo, "test", "blah", true) == false
+    end
+
+    test "aliases for true", %{repo: repo} do
+      check_true_alias(repo, "yes")
+      check_true_alias(repo, "yEs")
+      check_true_alias(repo, "on")
+      check_true_alias(repo, "ON")
+      check_true_alias(repo, "TrUe")
+      check_true_alias(repo, "1")
+    end
+
+    test "aliases for false", %{repo: repo} do
+      check_false_alias(repo, "no")
+      check_false_alias(repo, "nO")
+      check_false_alias(repo, "off")
+      check_false_alias(repo, "oFf")
+      check_false_alias(repo, "fAlSe")
+      check_false_alias(repo, "0")
+    end
+
+    test "multiple strings exist (no subsection)", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: nil,
+          name: "blah",
+          value: "true"
+        })
+
+      :ok =
+        Storage.add_config_entry(
+          repo,
+          %ConfigEntry{
+            section: "test",
+            subsection: nil,
+            name: "blah",
+            value: "false"
+          },
+          add?: true
+        )
+
+      assert Config.get_boolean(repo, "test", "blah", false) == false
+    end
+
+    test "multiple strings exist (with subsection)", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: "sub",
+          name: "blah",
+          value: "false"
+        })
+
+      :ok =
+        Storage.add_config_entry(
+          repo,
+          %ConfigEntry{
+            section: "test",
+            subsection: "sub",
+            name: "blah",
+            value: "true"
+          },
+          add?: true
+        )
+
+      assert Config.get_boolean(repo, "test", "sub", "blah", true) == true
+    end
+
+    test "true: value without =", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: "sub",
+          name: "blah",
+          value: nil
+        })
+
+      assert Config.get_boolean(repo, "test", "sub", "blah", false) == true
+    end
+
+    test "false: value with = but nothing further", %{repo: repo} do
+      :ok =
+        Storage.add_config_entry(repo, %ConfigEntry{
+          section: "test",
+          subsection: "sub",
+          name: "blah",
+          value: ""
+        })
+
+      assert Config.get_boolean(repo, "test", "sub", "blah", true) == false
+    end
+  end
+end
